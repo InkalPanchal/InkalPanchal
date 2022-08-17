bb2d19d0377c33155e5872def58bd1cc5be43556
InkalPanchal <panchalinkal15@gmail.com>
2022-08-17 20:58:00 +0530

    worked on header ui

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '', redirectTo: 'header', pathMatch: 'full'},
+  // {path: 'header', component:HeaderComponent, children:[{ path:'login', component:LoginComponent }]}
+
+    
+  ]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent
-  ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
+  ],
+  entryComponents: [LoginComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    CartComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
