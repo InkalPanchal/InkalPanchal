b9fd65749d2a3de3a8a22938a931e5f5e0a3ecfd
InkalPanchal <panchalinkal15@gmail.com>
2022-09-13 11:34:26 +0530

    Create tic-tac-toe using react

--- REACT/TICTACTOE/src/game.js
+++ REACT/TICTACTOE/src/game.js
@@ -1,16 +1,55 @@
 import React from "react";
 import Board from "./board";
+import CalculateWinner from "./components/CalculateWinner/CalculateWinner";
 
 class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      history: [{
+        squares : Array(9).fill(null),
+      }],
+      xIsNext: true,
+    }
+  }
+  handleclick(i){
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    console.log(current);
+    const squares = current.squares.slice();
+        if(CalculateWinner(squares) || squares[i]){
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+          history: history.concat([{
+            squares: squares,
+          }]),
+            xIsNext: !this.state.xIsNext,
+        })
+  }
     render(){
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = CalculateWinner(current.squares);
+
+      let status;
+      if(winner){
+        status = `Winner ${winner}`;
+      }else{
+        status = `Next Player: ${this.state.xIsNext ? 'X' : 'O'}`;
+      }
         return (
           <div className="game">
             <div className="game-board">
-                <Board />
+                <Board 
+                  squares = {current.squares}
+                  onClick={(i)=> this.handleclick(i)}
+                />
             </div>
             <div className="game-info">
-              <div></div>
-              <ol></ol>
+              <div>{status}</div>
+              <ol>{}</ol>
             </div>
           </div>  
         );
